303973e7b1e5427250c3a7756572ab9c0573921a
trev191 <trev191@yahoo.com>
2024-07-17 23:12:39 -0700

    Rename model fields

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -147,7 +147,7 @@ def show_exam_result(request, course_id, submission_id):
     total_score = 0
     for choice in choices:
       if choice.is_correct:
-          total_score += choice.question.grade_point
+          total_score += choice.question.grade
 
     context['course'] = course
     context['grade'] = total_score
